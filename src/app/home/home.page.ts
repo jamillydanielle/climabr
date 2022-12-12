dfd3f4bdaeea5e8254711903589b320de0f05b28
Jamilly <jamillydanieli@gmail.com>
2022-12-11 22:51:52 -0300

    [ DEFINIDO ] Classificações de IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -25,20 +25,44 @@ export class HomePage {
 
   async showIMC() {
     if (this.imc < 18.5) {
-      this.showIMC({
-        message: `IMC = ${this.imc.toFixed(2)} | Atenção: Você está magro!`,
-      })
+      const toast = await this.toastCtrl.create({
+        message: `IMC = ${this.imc.toFixed(2)} | Classificação: MAGREZA`,
+        duration: 3000,
+        color: 'secondary'
+      });
+      toast.present()
     }
-    
-    // const toast = await this.toastCtrl.create({
-    //   message: `IMC = ${this.imc.toFixed(2)}`,
-    //   duration: 3000,
-    //   color: 'secondary'
-    // })
-    
-    // toast.present()
-
+    else if (this.imc > 18.5 && this.imc < 24.9) {
+      const toast = await this.toastCtrl.create({
+        message: `IMC = ${this.imc.toFixed(2)} | Classificação: NORMAL`,
+        duration: 3000,
+        color: 'secondary'
+      });
+      toast.present()
+    }
+    else if (this.imc > 25.0 && this.imc < 29.9) {
+      const toast = await this.toastCtrl.create({
+        message: `IMC = ${this.imc.toFixed(2)} | Classificação: SOBREPESO`,
+        duration: 3000,
+        color: 'secondary'
+      });
+      toast.present()
+    }
+    else if (this.imc > 30.0 && this.imc < 39.9) {
+      const toast = await this.toastCtrl.create({
+        message: `IMC = ${this.imc.toFixed(2)} | Classificação: OBESIDADE`,
+        duration: 3000,
+        color: 'secondary'
+      });
+      toast.present()
+    }
+    else if (this.imc > 40.0) {
+      const toast = await this.toastCtrl.create({
+        message: `IMC = ${this.imc.toFixed(2)} | Classificação: OBESIDADE GRAVE`,
+        duration: 3000,
+        color: 'secondary'
+      });
+      toast.present()
+    }
   }
-
-
 }
